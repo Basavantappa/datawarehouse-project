d487b15d77e75d6cd9d8bcaabc59349f7f19fdc7
Basavantappa <puttibasavantappa@gmail.com>
2025-09-21 16:21:31 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,29 @@
+# Script to initialize the DWH DB with required schemas
 
+
+USE master
+GO
+
+-- Drop and Recreate the 'DWH' DB if already exists
+IF EXISTS (SELECT 1 FROM SYS.DATABASES WHERE name='DWH')
+BEGIN
+ALTER DATABASE DWH SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+DROP DATABASE DWH
+END
+
+-- Create the 'DWH' DB
+CREATE DATABASE DWH
+GO
+
+-- Create Schemas
+USE DWH
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
