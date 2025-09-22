5e4bf46248b429a8cc78e1c871828a086ae2f536
Basavantappa <puttibasavantappa@gmail.com>
2025-09-22 15:55:27 +0530

    Update sp_load_bronze.sql

--- scripts/bronze/sp_load_bronze.sql
+++ scripts/bronze/sp_load_bronze.sql
@@ -4,92 +4,99 @@ AS
 BEGIN
 SET NOCOUNT ON;
 
-DECLARE @starttime DATE;
-DECLARE @endtime DATE;
-DECLARE @TotalTimeSpent INT;
+BEGIN TRY
 
-SET @starttime =GETDATE(); -- Get the starting time
+	DECLARE @starttime DATE;
+	DECLARE @endtime DATE;
+	DECLARE @TotalTimeSpent INT;
 
+	SET @starttime =GETDATE(); -- Get the starting time
 
+	PRINT '==== Begin Loading Data to bronze layer tables ===='
 
-PRINT '==== Begin Loading Data to bronze layer tables ===='
+	PRINT '==> Loading cust_info Data'
+	TRUNCATE TABLE bronze.crm_cust_info;
+	BULK INSERT bronze.crm_cust_info
+	FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
 
-PRINT '==> Loading cust_info Data'
-TRUNCATE TABLE bronze.crm_cust_info;
-BULK INSERT bronze.crm_cust_info
-FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK 
+	);
 
-);
+	PRINT '=>Loading cust_info Data completed'
 
-PRINT '=>Loading cust_info Data completed'
+	PRINT '==> Loading prd_info Data'
+	TRUNCATE TABLE bronze.crm_prd_info;
+	BULK INSERT bronze.crm_prd_info
+	FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
 
-PRINT '==> Loading prd_info Data'
-TRUNCATE TABLE bronze.crm_prd_info;
-BULK INSERT bronze.crm_prd_info
-FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK 
+	);
+	PRINT '=>Loading prd_info Data completed'
 
-);
-PRINT '=>Loading prd_info Data completed'
+	PRINT '==> Loading sales_details Data'
+	TRUNCATE TABLE bronze.crm_sales_details;
+	BULK INSERT bronze.crm_sales_details
+	FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
 
-PRINT '==> Loading sales_details Data'
-TRUNCATE TABLE bronze.crm_sales_details;
-BULK INSERT bronze.crm_sales_details
-FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK 
+	);
+	PRINT '=>Loading sales_details Data completed'
 
-);
-PRINT '=>Loading sales_details Data completed'
+	PRINT '==> Loading cust_az12 Data'
+	TRUNCATE TABLE bronze.erp_cust_az12;
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
 
-PRINT '==> Loading cust_az12 Data'
-TRUNCATE TABLE bronze.erp_cust_az12;
-BULK INSERT bronze.erp_cust_az12
-FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK 
+	);
+	PRINT '=>Loading cust_az12 Data completed'
 
-);
-PRINT '=>Loading cust_az12 Data completed'
+	PRINT '==> Loading loc_a101 Data'
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
 
-PRINT '==> Loading loc_a101 Data'
-TRUNCATE TABLE bronze.erp_loc_a101;
-BULK INSERT bronze.erp_loc_a101
-FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK 
+	);
+	PRINT '=>Loading loc_a101 Data completed'
 
-);
-PRINT '=>Loading loc_a101 Data completed'
+	PRINT '==> Loading px_cat_g1v2 Data'
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
 
-PRINT '==> Loading px_cat_g1v2 Data'
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'F:\RT8U\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK 
+	);
+	PRINT '=>Loading px_cat_g1v2 Data completed'
 
-);
-PRINT '=>Loading px_cat_g1v2 Data completed'
+	PRINT '==== Loading Data to bronze layer completed ===='
 
-PRINT '==== Loading Data to bronze layer completed ===='
+	SET @endtime =GETDATE();
+	SET @TotalTimeSpent = DATEDIFF(MINUTE,@starttime,@endtime)
+	PRINT 'Total Time spent to loda data is ' + cast(@TotalTimeSpent as nvarchar(2))+ ' Minutes'
 
-SET @endtime =GETDATE();
-SET @TotalTimeSpent = DATEDIFF(MINUTE,@starttime,@endtime)
-PRINT 'Total Time spent to loda data is ' + cast(@TotalTimeSpent as nvarchar(2))+ ' Minutes'
+END TRY
+
+BEGIN CATCH
+	PRINT 'Failed to load data '
+END CATCH
+
 END
